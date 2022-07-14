bfe8076a01cc1e4d57bc49b46e7998fff052f99d
김호수 <44800134+hosukim@users.noreply.github.com>
2022-07-15 00:57:47 +0900

    FE : Apply Naver Map API (#1)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    let map = null;
+    const initMap = () => {
+      const map = new naver.maps.Map("map", {
+        center: new naver.maps.LatLng(37.511337, 127.012084),
+        zoom: 13,
+      });
+    };
+    initMap();
+  }, []);
+
+  //지도 사이즈 관련 스타일
+  const mapStyle = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div id="map" style={mapStyle}></div>
+    </React.Fragment>
   );
 }
 
