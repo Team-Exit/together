bc9254fdb8e42e0aaba812fd8b241e0dae53c196
Rohsooil <food869@naver.com>
2022-07-16 15:49:25 +0900

    BE : 백앤드 프로젝트 구축(#2)

---
+++ back/src/test/kotlin/com/exit/together/TogetherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exit.together
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
